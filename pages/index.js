7a36b043e470db4c1a6faf2b74b9848558907d47
Darya <ilinykhdarya@yandex.ru>
2022-06-29 14:50:12 +0500

    feat: сохранить и вставить данные из формы Новое место

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,3 @@
-
-// popup
-const userName = document.querySelector('.profile__name');
-const aboutUser = document.querySelector('.profile__profession');
-const buttonEditProfile = document.querySelector('.button_type_edit');
-const popupProfile = document.querySelector('.popup-profile');
-const formEditProfile = popupProfile.querySelector('form[name="formEditProfile"]');
-const formUserName = formEditProfile.querySelector('#userName');
-const formAboutUser = formEditProfile.querySelector('#aboutUser');
-const buttonSaveProfile = formEditProfile.querySelector('button[type="submit"]');
-const buttonCloseProfile = formEditProfile.querySelector('button[type="reset"]');
-
-buttonEditProfile.addEventListener('click', function(){openPopup(popupProfile)});
-buttonCloseProfile.addEventListener('click', closePopup);
-
-
-const buttonAddPlace = document.querySelector('.button_type_add');
-const popupNewPlace = document.querySelector('.popup-new-place');
-const formAddPlace = document.querySelector('form[name="formAddPlace"]');
-const placeName = formAddPlace.querySelector('#placeName');
-const placeLink = formAddPlace.querySelector('#placeLink');
-const buttonSavePlace = formAddPlace.querySelector('button[type="submit"]');
-const buttonClosePlace = formAddPlace.querySelector('button[type="reset"]');
-
-buttonAddPlace.addEventListener('click', function(){openPopup(popupNewPlace)});
-buttonClosePlace.addEventListener('click', closePopup);
-
-
-// открывает popup, в колбеке addEventListener передать параметр - эл-т popup
-function openPopup(popup){
-  popup.classList.add('popup_opened');
-  if (popup === popupProfile) {
-    formUserName.value = userName.textContent;
-    formAboutUser.value = aboutUser.textContent;
-  }
-};
-
-// закрывает popup
-function closePopup (){
-  this.closest('.popup').classList.remove('popup_opened');
-}
-
-// сохранить данные
-formEditProfile.addEventListener('submit', saveForm);
-
-function saveForm (evt) {
-  evt.preventDefault();
-
-  userName.textContent = formUserName.value;
-  aboutUser.textContent = formAboutUser.value;
-
-  this.closest('.popup').classList.remove('popup_opened'); //не получается вызвать функцию this.closePopup() ???
-}
-
-//наполнение сайта карточкама place
-
-//скопируем и наполним шаблон place
-const places = document.querySelector('.places'); // куда вставлять
-const placeTemplate = document.querySelector('#place').content; // содержимое шаблона
-
-//вот это надо делать в цикле для вставки первых 6 и вызывать функцию 1р при добавлении нового места
-function publicPlace(namePlace, linkPlace) {
-  console.log('name: ' + namePlace);
-  console.log('link: ' + linkPlace);
-  const placeElement = placeTemplate.querySelector('.place').cloneNode(true); //скопировали эл-т div .place
-  placeElement.querySelector('.place__name').textContent = namePlace;
-  placeElement.querySelector('.place__image').src = linkPlace;
-  places.prepend(placeElement);
-}
-
-//добавить место
-formAddPlace.addEventListener('submit', addNewPlace);
-
-function addNewPlace(evt){
-  evt.preventDefault();
-
-
-};
-
-// вставка 6 мест по шаблону при загрузке страницы
 const initialCards = [
   {
     name: 'Архыз',
@@ -105,13 +25,89 @@ const initialCards = [
   }
   ];
 
+  // вставить 6 мест при загрузке страницы из массива initialCards
   initialCards.forEach(function (card) {
     publicPlace (card.name, card.link);
   });
 
 
+// popup profile
+const userName = document.querySelector('.profile__name');
+const aboutUser = document.querySelector('.profile__profession');
+const buttonEditProfile = document.querySelector('.button_type_edit');
+const popupProfile = document.querySelector('.popup-profile');
+const formEditProfile = popupProfile.querySelector('form[name="formEditProfile"]');
+const formUserName = formEditProfile.querySelector('#userName');
+const formAboutUser = formEditProfile.querySelector('#aboutUser');
+const buttonCloseProfile = formEditProfile.querySelector('button[type="reset"]');
 
-  //удаление места
+buttonEditProfile.addEventListener('click', function(){openPopup(popupProfile)});
+buttonCloseProfile.addEventListener('click', closePopup);
+formEditProfile.addEventListener('submit', saveFormProfile);
+
+// popup place
+const buttonAddPlace = document.querySelector('.button_type_add');
+const popupNewPlace = document.querySelector('.popup-new-place');
+const formAddPlace = document.querySelector('form[name="formAddPlace"]');
+const buttonClosePlace = formAddPlace.querySelector('button[type="reset"]');
+
+buttonAddPlace.addEventListener('click', function(){openPopup(popupNewPlace)});
+buttonClosePlace.addEventListener('click', closePopup);
+formAddPlace.addEventListener('submit', addNewPlace);
+
+
+// открывает popup, в колбеке addEventListener передать параметр - эл-т popup
+function openPopup(popup){
+  popup.classList.add('popup_opened');
+  if (popup === popupProfile) {
+    formUserName.value = userName.textContent;
+    formAboutUser.value = aboutUser.textContent;
+  }
+};
+
+// закрывает popup
+function closePopup (){
+  this.closest('.popup').classList.remove('popup_opened');
+}
+
+// сохранить данные профиля
+function saveFormProfile (evt) {
+  evt.preventDefault();
+
+  userName.textContent = formUserName.value;
+  aboutUser.textContent = formAboutUser.value;
+
+  this.closest('.popup').classList.remove('popup_opened');
+
+}
+//наполнение сайта карточкама place
+
+//скопируем и наполним шаблон place
+const places = document.querySelector('.places'); // куда вставлять
+const placeTemplate = document.querySelector('#place').content; // содержимое шаблона
+
+//добавить на сайт новое место, в параметрах имя и ссылка (из массива или с формы)
+function publicPlace(namePlace, linkPlace) {
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true); //скопировали эл-т div .place
+  placeElement.querySelector('.place__name').textContent = namePlace;
+  placeElement.querySelector('.place__image').src = linkPlace;
+  places.prepend(placeElement);
+}
+
+// опубликовать карточку, взяв данные из формы
+function addNewPlace(evt){
+  evt.preventDefault();
+  const placeName = this.querySelector('#placeName').value;
+  const placeLink = this.querySelector('#placeLink').value;
+  publicPlace(placeName, placeLink);
+
+  this.querySelector('#placeName').value = '';
+  this.querySelector('#placeLink').value = '';
+
+  this.closest('.popup').classList.remove('popup_opened');
+};
+
+//удаление места
 
 
   // toggle liked
