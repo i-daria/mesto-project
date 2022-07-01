110d10068352bcb5909f7653edb054e19ffdadae
Darya <ilinykhdarya@yandex.ru>
2022-07-01 16:15:08 +0500

    fix: исправить все ошибки по ревью

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,11 @@ const buttonAddPlace = document.querySelector(".button_type_add");
 const popupNewPlace = document.querySelector(".popup-new-place");
 const formAddPlace = document.querySelector('form[name="formAddPlace"]');
 const buttonClosePlace = formAddPlace.querySelector('button[type="reset"]');
+const placeName = document.querySelector("#placeName");
+const placeLink = document.querySelector("#placeLink");
 
 //шаблон place
-const places = document.querySelector(".places"); // куда вставлять
+const placesContainer = document.querySelector(".places"); // куда вставлять
 const placeTemplate = document.querySelector("#place").content; // содержимое шаблона
 
 // popup gallery
@@ -55,45 +57,27 @@ const buttonCloseGallery = gallery.querySelector(".button_type_close");
 const galleryImage = gallery.querySelector(".popup__gallery-image");
 const galleryName = gallery.querySelector(".popup__gallery-name");
 
-buttonEditProfile.addEventListener("click", function () {
-  openPopup(popupProfile);
-});
-buttonCloseProfile.addEventListener("click", function () {
-  closePopup(buttonCloseProfile);
-});
-formEditProfile.addEventListener("submit", saveFormProfile);
-
-buttonAddPlace.addEventListener("click", function () {
-  openPopup(popupNewPlace);
-});
-buttonClosePlace.addEventListener("click", function () {
-  closePopup(buttonClosePlace);
-});
-formAddPlace.addEventListener("submit", addNewPlace);
-
-buttonCloseGallery.addEventListener("click", function () {
-  galleryImage.src = "#";
-  galleryName.textContent = "";
-  closePopup(this);
-});
-
 // вставить 6 мест при загрузке страницы из массива initialCards
 initialCards.forEach(function (card) {
-  publicPlace(card.name, card.link);
+  renderPlace(card.name, card.link);
 });
 
-// открыть popup, для формы профиля подставить данные со страницы
+// открыть popup
 function openPopup(popup) {
   popup.classList.add("popup_opened");
-  if (popup === popupProfile) {
-    formUserName.value = userName.textContent;
-    formAboutUser.value = userAbout.textContent;
-  }
 }
 
+// открыть popup profile
+function openPropfilePopup() {
+  formUserName.value = userName.textContent;
+  formAboutUser.value = userAbout.textContent;
+
+  openPopup(popupProfile);
+  }
+
 // закрыть popup
-function closePopup(element) {
-  element.closest(".popup").classList.remove("popup_opened");
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
 }
 
 // сохранить данные профиля
@@ -103,47 +87,71 @@ function saveFormProfile(evt) {
   userName.textContent = formUserName.value;
   userAbout.textContent = formAboutUser.value;
 
-  closePopup(this);
+  closePopup(popupProfile);
 }
 
 //добавить место, на входе данные из массива или с формы
-function publicPlace(namePlace, linkPlace) {
+function createPlace(namePlace, linkPlace) {
   const placeElement = placeTemplate.querySelector(".place").cloneNode(true); //скопировали эл-т div .place
+  const placeImage = placeElement.querySelector(".place__image");
   placeElement.querySelector(".place__name").textContent = namePlace;
-  placeElement.querySelector(".place__image").src = linkPlace;
-  placeElement.querySelector(".place__image").alt = namePlace;
-  places.prepend(placeElement);
+  placeImage.src = linkPlace;
+  placeImage.alt = namePlace;
 
-  placeElement.querySelector(".place__image").addEventListener("click", function () {
+  placeImage.addEventListener("click", function () {
       galleryImage.src = linkPlace;
+      galleryImage.alt = namePlace;
       galleryName.textContent = namePlace;
 
       openPopup(gallery);
     });
 
-  document.querySelector(".place__trash-icon").addEventListener("click", deletePlace);
-  document.querySelector(".place__like-icon").addEventListener("click", likePlace);
+  placeElement.querySelector(".place__trash-icon").addEventListener("click", deletePlace);
+  placeElement.querySelector(".place__like-icon").addEventListener("click", likePlace);
+
+  return placeElement;
 }
 
 // добавить место по данным формы
 function addNewPlace(evt) {
   evt.preventDefault();
-  const placeName = this.querySelector("#placeName").value;
-  const placeLink = this.querySelector("#placeLink").value;
-  publicPlace(placeName, placeLink);
+  renderPlace(placeName.value, placeLink.value);
 
-  this.querySelector("#placeName").value = "";
-  this.querySelector("#placeLink").value = "";
+  formAddPlace.reset();
 
-  closePopup(this);
+  closePopup(popupNewPlace);
+}
+
+// опубликовать new place
+function renderPlace (namePlace, linkPlace) {
+  const element = createPlace(namePlace, linkPlace);
+  placesContainer.prepend(element);
 }
 
 //удалить карточку место
-function deletePlace() {
-  const cardPlace = this.closest(".place").remove();
+function deletePlace(evt) {
+  const cardPlace = evt.target.closest(".place").remove();
 }
 
 // переключатель нравится / не нравится
 function likePlace(evt) {
   evt.target.classList.toggle("liked");
 }
+
+buttonEditProfile.addEventListener("click", openPropfilePopup);
+buttonCloseProfile.addEventListener("click", function () {
+  closePopup(popupProfile);
+});
+formEditProfile.addEventListener("submit", saveFormProfile);
+
+buttonAddPlace.addEventListener("click", function () {
+  openPopup(popupNewPlace);
+});
+buttonClosePlace.addEventListener("click", function () {
+  closePopup(popupNewPlace);
+});
+formAddPlace.addEventListener("submit", addNewPlace);
+
+buttonCloseGallery.addEventListener("click", function (evt) {
+  closePopup(evt.target.closest(".popup"));
+});
